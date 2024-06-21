63c1cac54f3c97061052c57c13f59f15c9645051
suha27 <suha.gdl@gmail.com>
2024-06-21 13:19:31 -0400

    Added src and test

---
+++ src/test/java/io/javatest/MathUtilsTest.java
@@ -0,0 +1,19 @@
+package io.javatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		MathUtils mathUtils=new MathUtils();
+		
+		int expected = 1;
+		int actual = mathUtils.add(1, 1);
+		
+		assertEquals(expected, actual);
+	}
+
+}
