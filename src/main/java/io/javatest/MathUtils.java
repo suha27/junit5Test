d2b4b6c8362fa6dcc9c9d7c68886a9b70b5919d8
Suhasini <suha.gdl@gmail.com>
2024-06-21 22:10:34 -0400

    Updated and added test case Signed-off-by: Suhasini <suha.gdl@gmail.com>

--- src/main/java/io/javatest/MathUtils.java
+++ src/main/java/io/javatest/MathUtils.java
@@ -5,4 +5,20 @@ public class MathUtils {
 	public int add(int a , int b) {
 		return a+b;
 	}
+	
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	
+	public double computeCircleArea(double radis) {
+		return Math.PI * radis * radis;
+	}
 }
--- src/test/java/io/javatest/MathUtilsTest.java
+++ src/test/java/io/javatest/MathUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class MathUtilsTest {
 
 	@Test
-	void test() {
+	void testAdd() {
 		MathUtils mathUtils=new MathUtils();
 		
 		int expected = 2;
@@ -15,5 +15,41 @@ class MathUtilsTest {
 		
 		assertEquals(expected, actual);
 	}
-
+	
+	@Test
+	void testAddFailure() {
+		MathUtils mathUtils=new MathUtils();
+		
+		int expected = 3;
+		int actual = mathUtils.add(1, 1);
+		
+		assertNotEquals(expected, actual);
+	}
+	
+	@Test
+	void testDivide() {
+		MathUtils mathUtils = new MathUtils();
+		
+		assertThrows(ArithmeticException.class, 
+				()->	mathUtils.divide(1, 0)
+				,"Divide by Zero");
+	}
+	
+	@Test
+	void testDivideFailure() {
+		MathUtils mathUtils = new MathUtils();
+		
+		assertThrows(ArithmeticException.class, 
+				()->	mathUtils.divide(1, 10)
+				,"Divide by Zero");
+	}
+	
+	@Test
+	void testComputeCircle() {
+		MathUtils mathUtils=new MathUtils();
+		
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Area of circle");
+	}
+	
+	
 }
